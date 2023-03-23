8264d6b047983d63cd21666f1a8747d3c012d495
kaanuyann@gmail.com <kaanuyann@gmail.com>
2023-03-23 18:16:34 +0300

    Raycast Shooting Added, Some scripts combined

---
+++ Assets/Scripts/Weapons/VectorsExtension.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public static class VectorsExtension
+{
+    public static Vector3 WithAxis(this Vector3 vector, Axis axis, float value)
+    {
+        return new Vector3(
+            axis == Axis.X ? value : vector.x,
+            axis == Axis.Y ? value : vector.y,
+            axis == Axis.Z ? value : vector.z
+        );
+    }
+}
+
+public enum Axis
+{
+    X,
+    Y,
+    Z
+}
